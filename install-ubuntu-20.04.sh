e290c0669b2caf4f7954361dc790436fc69116a3
Egor Suvorov <egor.suvorov@gmail.com>
2021-03-31 13:59:29 +0300

    Update install-ubuntu-20.04.sh

--- install-ubuntu-20.04.sh
+++ install-ubuntu-20.04.sh
@@ -3,9 +3,9 @@ set -uexo pipefile
 sudo apt update
 sudo apt upgrade
 sudo apt install \
-    aptitude nano git tmux p7zip-full \
+    aptitude nano vim git tmux p7zip-full \
     valgrind cmake make cppcheck clang-format-10 clang-tidy-10 \
     libboost-all-dev \
     g++-10 \
-    clang-10
+    clang-10 \
     libc++-10-dev libc++abi-10-dev
