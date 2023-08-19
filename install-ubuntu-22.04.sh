d39743fa57af3504f9bbfb42805c9cc663d2e101
Egor Suvorov <egor.suvorov@gmail.com>
2023-08-19 15:20:15 +0300

    Update install-ubuntu-22.04.sh

--- install-ubuntu-22.04.sh
+++ install-ubuntu-22.04.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt upgrade
 sudo apt install \
     aptitude nano vim git tmux p7zip-full curl netcat-openbsd jq \
-    valgrind cmake make cppcheck clang-format-14 clang-tidy-14 \
+    valgrind cmake make cppcheck clang-format-15 clang-tidy-15 \
     libboost-all-dev \
     g++-12 \
-    clang-14 \
-    libc++-14-dev libc++abi-14-dev
+    clang-15 \
+    libc++-15-dev libc++abi-15-dev
